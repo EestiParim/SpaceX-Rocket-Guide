2f3c3635066a21f6a7ba976ba6d72de37ef9dbc8
Tõnis Piirits <olen2ge@gmail.com>
2018-08-08 00:03:02 +0300

    added filtering options

--- app/src/main/java/com/example/seep/spacexapp/MainActivity.kt
+++ app/src/main/java/com/example/seep/spacexapp/MainActivity.kt
@@ -5,17 +5,18 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
-import android.widget.LinearLayout
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import android.os.StrictMode
+import android.widget.*
+
 
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        val emptyRocketList : List<RocketDataEntry> = emptyList()
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
         super.onCreate(savedInstanceState)
@@ -30,16 +31,31 @@ class MainActivity : AppCompatActivity() {
                 .build()
 
         val spaceXApi = retrofit.create(SpaceXApi::class.java)
-        val asi = spaceXApi.getTop().execute().body()
+        var asi = spaceXApi.getTop().execute().body()
+
+        /*var filteredAsi : List<RocketDataEntry> = asi?.filter{it.active}*/
         Log.d("WOW", asi.toString())
-
-
         val rv = findViewById<RecyclerView>(R.id.recycle_view)
-        rv.layoutManager = LinearLayoutManager(this,LinearLayout.VERTICAL, false);
-        val rockets = ArrayList<Rocket>()
+        rv.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+/*        val rockets = ArrayList<Rocket>()
         rockets.add(Rocket("SpaceX First","22 Engines", "Russia"))
-        rockets.add(Rocket("Big Large Falcon","5 Engines", "Ireland Republic"))
+        rockets.add(Rocket("Big Large Falcon","5 Engines", "Ireland Republic"))*/
         var adapter = RocketListAdapter(asi)
         rv.adapter = adapter
+        adapter.notifyDataSetChanged()
+
+        val toggleBtnActive = findViewById<ToggleButton>(R.id.toggle_active)
+        toggleBtnActive?.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                val filteredAsiInActive = asi?.filter {it.active }
+                rv.swapAdapter(RocketListAdapter(filteredAsiInActive), false)
+            }
+            else{
+                rv.swapAdapter(adapter,false)
+            }
+            /*val msg = "Toggle Button is " + if (isChecked) "ON" else "OFF"
+            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_LONG).show()*/
+        }
     }
+
 }
